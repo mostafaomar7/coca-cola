ddfb5269b87c19665d9e74acf762ba103442caba
unknown <you@example.com>
2021-12-07 15:49:03 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+//toogle class on icon
+
+let x= document.querySelector(".toogle i");
+x.onclick = function() {
+    this.classList.toggle("fa-spin")
+    document.querySelector(".setting-box").classList.toggle("open")
+}
+
+
+
+
 let landingpage = document.querySelector(".landing-page")
 let imgArray = ["01.jpg","02.jpg","03.jpg","04.jpg","05.jpg"]
 
